bee531d6135f33ec9ab7d4e3d50e1ecb04133e24
Boyan5 <b.milanov5@abv.bg>
2021-08-03 16:17:57 +0300

    SortVisualize

---
+++ SortVisualize/SortVisualize/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SortVisualize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxNums = new System.Windows.Forms.TextBox();
+            this.lblInformation = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxNums
+            // 
+            this.txtBoxNums.Location = new System.Drawing.Point(107, 360);
+            this.txtBoxNums.Name = "txtBoxNums";
+            this.txtBoxNums.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxNums.TabIndex = 0;
+            this.txtBoxNums.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxNums_KeyPress);
+            // 
+            // lblInformation
+            // 
+            this.lblInformation.AutoSize = true;
+            this.lblInformation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblInformation.Location = new System.Drawing.Point(477, 349);
+            this.lblInformation.Name = "lblInformation";
+            this.lblInformation.Size = new System.Drawing.Size(0, 20);
+            this.lblInformation.TabIndex = 1;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(576, 303);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 450);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.lblInformation);
+            this.Controls.Add(this.txtBoxNums);
+            this.Name = "Form1";
+            this.Text = "Visualize";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxNums;
+        private System.Windows.Forms.Label lblInformation;
+        private System.Windows.Forms.Button btnNext;
+    }
+}
+
